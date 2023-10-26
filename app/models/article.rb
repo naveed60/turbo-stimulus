2fe3c0496f44a41ec2df0232d61d895f2f360dbb
naveed60 <advmiannaveedali@gmail.com>
2023-10-26 16:23:44 +0500

    initial setup

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+  has_one_attached :image
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
