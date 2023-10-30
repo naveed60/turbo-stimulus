1f4b6f215df930dd9fcc6a9230fe291c7c670590
naveed60 <advmiannaveedali@gmail.com>
2023-10-30 18:55:00 +0500

    bootstrap modal form

--- app/javascript/controllers/employee_modal_controller.js
+++ app/javascript/controllers/employee_modal_controller.js
@@ -5,4 +5,20 @@ export default class extends Controller {
   connect() {
     console.log('im connected!!!')
   }
+
+  initialize(){
+    this.element.setAttribute("data-action", "click->emloyee-modal#showModal")
+  }
+
+  showModal(event){
+    event.preventDefault()
+       this.url = this.element.getAttribute("href")
+       fetch(this.url, {
+        headrs:{
+          Accept: "text/vnd.turbo-stream.html"
+        }
+       })
+       .then(response => response.text()) 
+       .then(html => Turbo.renderStreamMessage(html))
+  }
 }
