8ad128b1c9ec814a4482adbb74ae7c7c78c6e53d
naveed60 <advmiannaveedali@gmail.com>
2023-12-04 18:33:58 +0500

    movies schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_050525) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_132937) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -77,6 +77,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_050525) do
   create_table "listings", force: :cascade do |t|
     t.text "title"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
