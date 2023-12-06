3d67019833ae4f711f95bcf2d8f49f6cfd4d496c
naveed60 <advmiannaveedali@gmail.com>
2023-12-06 18:36:22 +0500

    food schema added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_132937) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_06_133313) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -74,6 +74,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_132937) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "foods", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "listings", force: :cascade do |t|
     t.text "title"
     t.string "description"
@@ -94,6 +99,11 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_132937) do
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
